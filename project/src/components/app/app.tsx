2ca4865e27e426e6c3e938952d32202ddf3a7dcc
Alexander <bdiyoz@gmail.com>
2022-03-11 03:22:20 +0400

    refactor: correct misspell of folder favorites

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,7 +4,7 @@ import {
   Route
 } from 'react-router-dom';
 import { AppRoutes, AuthorizationStatus } from '../../const';
-import Favorites from '../favoriets/favorites';
+import Favorites from '../favorites/favorites';
 import Login from '../login/login';
 import Main from '../main/main';
 import Property from '../property/property';
